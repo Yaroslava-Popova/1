2d36a0407cd9425d1753802cf0663cd0324189d6
Yaroslava-Popova <st132757@student.spbu.ru>
2024-09-16 15:52:33 +0300

    создание readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-
+# Demo project #1
+# Popova Yaroslava Olegovna 24.B82-mm st132757@student.spbu.ru
+# Build
+make
+# Run
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+/* Popova Yaroslava st132757@student.spbu.ru first project
+*/
+#include <iostream>
+int main(int argc, char *argv[])
+{
+std::cout<<"Hello, world"<<std::endl;
+}
